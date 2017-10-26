aca80a05ca4dea91985e105a867961ed0eed91b5
kalibani <kalibani.ka@gmail.com>
2017-10-26 19:34:03 +0700

    form-login done

--- client/script.js
+++ client/script.js
@@ -1,10 +1,41 @@
-Vue.component('my-component', {
-  template: `<span>{{ message vue }}</span>`,
-  data: {
-    message: 'hello'
-  }
-})
+Vue.component('form-login', {
+  template: `
+<div>
+  <br> <br> <br> <br>
+  <div class="ui middle aligned center aligned grid">
+    <div class="column">
+      <h2 class="ui teal image header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png" class="ui large image">
+        <div class="content">
+          Log-in to your account
+        </div>
+      </h2>
+      <form class="ui large form">
+        <div class="ui stacked segment">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input type="text" name="email" placeholder="E-mail address">
+            </div>
+          </div>
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="lock icon"></i>
+              <input type="password" name="password" placeholder="Password">
+            </div>
+          </div>
+          <div class="ui fluid large teal submit button">Login</div>
+        </div>
+        <div class="ui error message"></div>
+      </form>
+      <div class="ui message">
+        New to us? <a href="#">Sign Up</a>
+      </div>
+    </div>
+  </div>
+  
+</div>`,})
 
-var app = new Vue({
+new Vue({
   el: '#app',
 })
